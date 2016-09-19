4d11dcc0d2b17d13ca6e18eae3f5dd46fa06a223
petermichuncc <plarrimo@uncc.edu>
2016-09-18 23:28:07 -0400

    Started working on making the css better and a profile page.

--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -12,7 +12,8 @@ angular.module('chatApp')
         Auth.createUser({
           name: $scope.user.name,
           email: $scope.user.email,
-          password: $scope.user.password
+          password: $scope.user.password,
+          
         })
         .then( function() {
           // Reconnect to socket as logged in user
