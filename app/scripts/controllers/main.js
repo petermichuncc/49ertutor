2463331ac86db7e210dce1461d205666325377ad
petermichuncc <plarrimo@uncc.edu>
2016-10-19 17:16:11 -0400

    Added a function to grab the name of the user of the message that was clicked in the chat.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,7 +5,12 @@ angular.module('chatApp')
   .controller('MainCtrl', function ($scope, chatroom) {
     $scope.getMessages = chatroom.getMessages;
     $scope.getVisitors = chatroom.getVisitors;
-
+$scope.showDetails = function(y) {
+     //use y here
+     
+     console.log("this is the clicked text " + y)
+     alert(y);
+}
     $scope.sendMessage = function () {
       if(!$scope.newMessage) {
         return;
